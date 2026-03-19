7839ffa5ab197814219e04b861908ef825ad6834
samyakbhattarai <bhattaraisamyak07@gmail.com>
2026-03-19 14:51:33 +0545

    first comit

---
+++ Text_Processing/textprocess.py
@@ -0,0 +1,17 @@
+from nltk import word_tokenize
+
+
+sentence = """The world is a very beautiful place, but the world I want to create is not
+        this. So lets just work harder for a new world"""
+
+
+print(word_tokenize(sentence))
+
+#Reading files 
+#Cleaning text
+#Tokenization
+#Basic NLP
+
+
+#Task:
+    #Load a text file, count words and find the most frequent words
