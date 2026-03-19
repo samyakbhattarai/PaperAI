f23a88df53085f130a2a0af41d4192617592663c
samyakbhattarai <bhattaraisamyak07@gmail.com>
2026-03-19 16:23:29 +0545

    read from a file, created tokens, tags and chunks entities

--- Text_Processing/textprocess.py
+++ Text_Processing/textprocess.py
@@ -1,13 +1,24 @@
-from nltk import word_tokenize
+from nltk import word_tokenize,pos_tag, ne_chunk
+from nltk.stem import PorterStemmer
 
-
+porter = PorterStemmer()
 sentence = """The world is a very beautiful place, but the world I want to create is not
         this. So lets just work harder for a new world"""
 
 
-print(word_tokenize(sentence))
 
 #Reading files 
+file = open("a_text_file.txt", "r")
+every_thing = ""
+word = []
+for ch in file:
+    every_thing+=ch
+
+every_thing = every_thing.lower()
+tokens = word_tokenize(every_thing)
+tags = pos_tag(tokens)
+entities = ne_chunk(tags)
+print(entities)
 #Cleaning text
 #Tokenization
 #Basic NLP
